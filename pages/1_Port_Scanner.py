f90252ebe08528df05e3e0b4ac4722303e0a8bb0
guptavikrant856-tech <guptavikrant856@gmail.com>
2026-06-07 22:47:41 +0530

    Update 1_Port_Scanner.py

--- pages/1_Port_Scanner.py
+++ pages/1_Port_Scanner.py
@@ -3,26 +3,58 @@ import socket
 
 st.title("🔍 Port Scanner")
 
-host = st.text_input("Target Host", "127.0.0.1")
+host = st.text_input("Target Host", "scanme.nmap.org")
 
-if st.button("Scan Ports"):
+start_port = st.number_input(
+    "Start Port",
+    min_value=1,
+    max_value=65535,
+    value=1
+)
+
+end_port = st.number_input(
+    "End Port",
+    min_value=1,
+    max_value=65535,
+    value=100
+)
+
+if st.button("Scan"):
 
     results = []
 
     with st.spinner("Scanning..."):
 
-        for port in range(1, 101):
+        for port in range(start_port, end_port + 1):
 
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.settimeout(0.3)
+            try:
+                s = socket.socket(
+                    socket.AF_INET,
+                    socket.SOCK_STREAM
+                )
 
-            result = s.connect_ex((host, port))
+                s.settimeout(0.5)
 
-            if result == 0:
-                results.append(f"❌ Port {port} OPEN")
-            else:
-                results.append(f"✅ Port {port} CLOSED")
+                result = s.connect_ex((host, port))
 
-            s.close()
+                if result == 0:
+                    results.append(
+                        f"❌ Port {port} OPEN"
+                    )
 
-    st.text("\n".join(results))
+                s.close()
+
+            except:
+                pass
+
+    if results:
+        st.success(
+            f"{len(results)} open ports found"
+        )
+
+        st.text("\n".join(results))
+
+    else:
+        st.success(
+            "No open ports found"
+        )
